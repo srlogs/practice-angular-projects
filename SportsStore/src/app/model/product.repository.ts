04943ebfad67e632e5ccd5e6b35e1abe2a39a1c2
srlogs <logeshwaran.sr1@gmail.com>
2021-05-13 20:31:13 +0530

    add store component

--- SportsStore/src/app/model/product.repository.ts
+++ SportsStore/src/app/model/product.repository.ts
@@ -6,19 +6,21 @@ export class ProductRepository {
     private products: Product[] = [];
     private categories: string[] = [];
     constructor(private dataSource: StaticDataSource) {
-        dataSource.getProducts().subscribe(data => {
+        this.dataSource.getProducts().subscribe((data: any) => {
             this.products = data;
-            this.categories = data.map(p => p.category)
-                .filter((c, index, array) => array.indexOf(c) == index).sort();
+            this.categories = data.map((p: any) => p.category)
+                .filter((c: any, index: number, array: any) => array.indexOf(c) == index).sort();
         });
     }
-    getProducts(category: string = null): Product[] {
+    getProducts(category: string = ""): Product[] {
         return this.products
             .filter(p => category == null || category == p.category);
     }
+
     getProduct(id: number): Product {
-        return this.products.find(p => p.id == id);
+        return this.products.find((p: any) => p.id == id);
     }
+
     getCategories(): string[] {
         return this.categories;
     }
