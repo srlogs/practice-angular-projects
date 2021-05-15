f0d2bb805e38c4b8bd09e43e1e0d3ea986b73dee
srlogs <logeshwaran.sr1@gmail.com>
2021-05-15 11:18:34 +0530

    add rest datasource and make http requests

--- SportsStore/src/app/model/model.module.ts
+++ SportsStore/src/app/model/model.module.ts
@@ -1,12 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { Cart } from './cart.model';
 import { Order } from './order.model';
 import { OrderRepository } from './order.repository';
 import { ProductRepository } from './product.repository';
+import { RestDataSource } from './rest.datasource';
 import { StaticDataSource } from './static.datasource';
 
 @NgModule({
-    providers: [ProductRepository, StaticDataSource, Cart, Order, OrderRepository]
+    imports: [HttpClientModule],
+    providers: [ProductRepository, StaticDataSource, Cart, Order, OrderRepository, { provide: StaticDataSource, useClass: RestDataSource }]
 })
 
 export class ModelModule { }
