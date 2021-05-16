9bdc5d316b32ce1fe3053b75d263d45c1a3735c0
srlogs <logeshwaran.sr1@gmail.com>
2021-05-16 20:54:04 +0530

    add admin featuresd methods and update rest datasource

--- SportsStore/src/app/model/rest.datasource.ts
+++ SportsStore/src/app/model/rest.datasource.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { HttpHeaders } from '@angular/common/http';
 
 import { Product } from './product.model';
-import { Cart } from './cart.model';
 import { Order } from './order.model';
 
 const PROTOCOL = 'http';
@@ -35,5 +35,37 @@ export class RestDataSource {
             this.auth_token = response.success ? response.token : null;
             return response.success;
         }));
+    }
+
+    saveProduct(product: Product): Observable<Product> {
+        return this.http.post<Product>(this.baseUrl + "products", product, this.getOptions());
+    }
+
+    updateProduct(product: Product): Observable<Product> {
+        return this.http.put<Product>(`${this.baseUrl}products/${product.id}`, product, this.getOptions());
+    }
+
+    deleteProduct(id: number): Observable<Product> {
+        return this.http.delete<Product>(`${this.baseUrl}products/{id}`, this.getOptions());
+    }
+
+    getOrders(): Observable<Order[]> {
+        return this.http.get<Order[]>(this.baseUrl + "orders", this.getOptions());
+    }
+
+    deleteOrder(id: number): Observable<Order> {
+        return this.http.delete<Order>(`${this.baseUrl}orders/${id}`,
+            this.getOptions());
+    }
+
+    updateOrder(order: Order): Observable<Order> {
+        return this.http.put<Order>(`${this.baseUrl}orders/${order.id}`,
+            order, this.getOptions());
+    }
+
+    private getOptions() {
+        return {
+            headers: new HttpHeaders({ "Authorization": `Bearer<${this.auth_token}>` })
+        }
     }
 }
