bbe3a82af76e8435c8fbecb690ef20931ecbc3f7
srlogs <logeshwaran.sr1@gmail.com>
2021-05-17 09:38:26 +0530

    add product editor and order table component

--- SportsStore/src/app/admin/orderTable.component.ts
+++ SportsStore/src/app/admin/orderTable.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { Order } from '../model/order.model';
+import { OrderRepository } from '../model/order.repository';
 
 @Component({
     templateUrl: './orderTable.component.html'
 })
 
 export class OrderTableComponent {
+    includeShipped: boolean = false;
+    constructor(private repository: OrderRepository) { }
 
+    getOrders(): Order[] {
+        return this.repository.getOrders().filter(o => !o.shipped || this.includeShipped);
+    }
+
+    markShipped(order: Order) {
+        order.shipped = true;
+        this.repository.updateOrder(order);
+    }
+
+    delete(id: number = 0) {
+        this.repository.deleteOrder(id);
+    }
 }
